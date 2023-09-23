87e7b18e2a6096eff02a9651bbee7ccf9d2871f7
Fernandoakafox <fernando_contas@yahoo.com.br>
2023-09-23 11:38:17 -0300

    Bug da função simulacao foi consertado. Estatisticas foram implementadas na funcao simulacao. As saidas de texto foram formatadas.

--- waitwise.c
+++ waitwise.c
@@ -41,18 +41,18 @@ int main(){
     simulacao(&velocidade, &ciclos, &clientesPorCiclo, &filaClientes);
 }
 void entradaDados(int *ciclos, int *clientesPorCiclo){
-    printf("##########################  Bem-vindo ao WaitWise  ##########################");
+    printf("#####################################  Bem-vindo ao WaitWise  #####################################");
     printf(" \n");
     printf(" \n");
-    printf("\nInsira a quantidade de simualção (ciclos).....................: ");
+    printf("\n        Insira a quantidade de simualção (ciclos).....................: ");
     scanf("%i", &(*ciclos));
-    printf("Insira a quantidade de entrada de clientes na fila (por ciclo): ");
+    printf("        Insira a quantidade de entrada de clientes na fila (por ciclo): ");
     scanf("%i", &(*clientesPorCiclo));
 }
 void inicializaFila(dNODO **filaClientes){
     dNODO *ptDescritor = (dNODO*) malloc(sizeof(dNODO));
     if(ptDescritor != NULL){ //se há espaço disponivel na memoria para alocar o ponteiro ptDescritor
-       ptDescritor->inicio = NULL; //inicializa cabeça da fista
+       ptDescritor->inicio = NULL; //inicializa cabeça da fila
        ptDescritor->fim = NULL;    //inicializa cauda da fila
        *filaClientes = ptDescritor; //cabeça da fila passa a apontar para um endereço que possui um ponteiro para o inicio e outro para o fim da fila
     }else{
@@ -70,6 +70,7 @@ void insereNaFila(dNODO **filaClientes, int numeroDoCliente){
            (*filaClientes)->inicio = novo;
            novo->prox = NULL;
         }else{
+            novo->info.cliente = numeroDoCliente;
            (*filaClientes)->fim->prox = novo; //o nodo que esta no fim da fila, passa a apontar para o novo nodo
            (*filaClientes)->fim = novo; //o fim da fila passa a apontar para o novo fim da fila, ou seja, passa a apontar para o nodo novo
            novo->prox = NULL; //o novo fim da fila passa a apontar para NULL
@@ -109,24 +110,35 @@ int retiraDaFila(dNODO **filaClientes){
 //esta função deve fazer a simulação de uma fila de supermercado, mostrando os ciclos de entrada de clientes,
 //a saida dos clientes e por fim deve apresentar os resuldados da simulação, isto é, as estatisticas
 void simulacao(int *velocidade, int *ciclos, int *clientesPorCiclo, dNODO **filaClientes){
-    int clientes = 0; //conta numero de clientes que entraram
-    int ciclosExecutados = 0; //conta o numero de ciclos executados após a ultima saida de um cliente, ou seja, sempre é zerado depois da saida de um cliente
+    int clientesAtendidos = 0; //contador, conta o numero de clientes que foram atendidos
+    int clientes = 0; //contador, conta numero de clientes que entraram
+    int ciclosExecutados = 0; //contador, conta o numero de ciclos executados após a ultima saida de um cliente, ou seja, sempre é zerado depois da saida de um cliente
     int i, j, clienteQueSaiu;
     for(i = 0; i < *ciclos; i++){
-        printf("\nCiclo[%i]", i+1);
+        printf("\n        Ciclo[%i]", i+1);
         for(j = 0; j < *clientesPorCiclo; j++){
-            insereNaFila(filaClientes, j+1);
-            printf("\nEntrou na fila o cliente: %i", clientes+1);
+            insereNaFila(filaClientes, clientes+1);
+            printf("\n        Entrou na fila o cliente: %i", clientes+1);
             clientes++;
         }
         ciclosExecutados++;
         if (ciclosExecutados == *velocidade){ //verifica se esta na hora de tirar alguem da fila, conforme velocidade definida pela funcao velocidadeDeAtendimento
             clienteQueSaiu = retiraDaFila(filaClientes);
-            printf("\nSaiu o cliente %i", clienteQueSaiu);
+            printf("\n        Saiu o cliente %i", clienteQueSaiu);
+            clientesAtendidos++;
             ciclosExecutados = 0;
         }
     }
+    printf(" \n");
+    printf(" \n");
+    printf("##########################  Resultados da simulação da fila do WaitWIse  ##########################\n");
     printf("\n");
+    printf("        Inicio da fila [%p].: %i\n", &(*filaClientes)->inicio,(*filaClientes)->inicio->info.cliente);
+    printf("        Fim da fila    [%p].: %i\n", &(*filaClientes)->fim,(*filaClientes)->fim->info.cliente);
+    printf(" \n");
+    printf("        Numero de clientes atendidos........: %i\n", clientesAtendidos);
+    printf("        Numero de clientes restantes na fila: %i\n", clientes - clientesAtendidos); //FAZER acho que é clientes - clientesAtendidos
+    printf("        Tempo de espera na fila(ciclos).....: %i\n", *velocidade);
 }
 //esta funcao deve retornar um numero aleatorio, que representará a velocidade com que os clientes saem da fila
 int velocidadeDeAtendimento(){
