feeb28479c9a9fbd8d866dfaa988e5d4bb80f992
Fernandoakafox <fernandoschwengber@mx2.unisc.br>
2023-09-22 22:03:26 -0300

    Funcão inicializaFila foi corrigida, função insereNaFila foi criada.

--- waitwise.c
+++ waitwise.c
@@ -3,21 +3,21 @@
 /***********************************************/ 
 #include <stdio.h>
 #include <stdlib.h>
-
 /***********************************************/ 
 /* Definição dos Registros                     */
 /***********************************************/ 
 typedef struct {           // registro para uma pessoa
-       int cliente;
+    int cliente;
 } INFORMACAO;
        
 typedef struct nodo {
-       INFORMACAO info;    // dados do registro
-       struct nodo* prox;  // ponteiro para o próximo registro
+    INFORMACAO info;    // dados do registro
+    struct nodo* prox;  // ponteiro para o próximo registro
 } NODO; 
+
 typedef struct {
-       NODO* inicio;      // ponteiro para o início da estrutura
-       NODO* fim;         // ponteiro para o fim da estrutura
+    NODO* inicio;      // ponteiro para o início da estrutura
+    NODO* fim;         // ponteiro para o fim da estrutura
 } dNODO;                  // com descritor
 
 /***********************************************/ 
@@ -25,6 +25,7 @@ typedef struct {
 /***********************************************/ 
 void entradaDados(int *ciclos, int *ciclo);
 void inicializaFila(dNODO **filaClientes);
+void insereNaFila(dNODO **filaClientes);
 
 int main(){
     int ciclo = 0;  //Tempo de simulação
@@ -32,8 +33,7 @@ int main(){
     entradaDados(&ciclos, &ciclo);
     dNODO *filaClientes;
     inicializaFila(&filaClientes);
-
-
+    insereNaFila(&filaClientes);
 }
 void entradaDados(int *ciclos, int *ciclo){
     printf("##########################  Bem-vindo ao WaitWise  ##########################");
@@ -45,6 +45,31 @@ void entradaDados(int *ciclos, int *ciclo){
     scanf("%i", &(*ciclo));
 }
 void inicializaFila(dNODO **filaClientes){
-       (*filaClientes)->inicio = NULL; //inicializa cabeça da fista
-       (*filaClientes)->fim = NULL;    //inicializa cauda da fila
+    dNODO *ptDescritor = (dNODO*) malloc(sizeof(dNODO));
+    if(ptDescritor != NULL){ //se há espaço disponivel na memoria para alocar o ponteiro ptDescritor
+       ptDescritor->inicio = NULL; //inicializa cabeça da fista
+       ptDescritor->fim = NULL;    //inicializa cauda da fila
+       *filaClientes = ptDescritor; //cabeça da fila passa a apontar para um endereço que possui um ponteiro para o inicio e outro para o fim da fila
+    }else{
+       printf("Espaço de memoria insuficiente!");
+       return;
+    }
+}
+//insere registro no fim da fila
+void insereNaFila(dNODO **filaClientes){
+    NODO *novo = (NODO*) malloc(sizeof(NODO));
+    if(novo != NULL){ //se ouver espaço suficiente na memoria para criar um novo nodo
+       if((*filaClientes)->fim == NULL){//Se a fila não possuir nodo algum
+           (*filaClientes)->fim = novo;
+           (*filaClientes)->inicio = novo;
+           novo->prox = NULL;
+        }else{
+           (*filaClientes)->fim->prox = novo; //o nodo que esta no fim da fila, passa a apontar para o novo nodo
+           (*filaClientes)->fim = novo; //o fim da fila passa a apontar para o novo fim da fila, ou seja, passa a apontar para o nodo novo
+           novo->prox = NULL; //o novo fim da fila passa a apontar para NULL
+        }
+    }else{
+        printf("Espaço de memoria insuficiente!");
+        return;
+    }
 }
